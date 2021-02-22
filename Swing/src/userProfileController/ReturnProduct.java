5b307db8215e932aaf75c6415d1a4db6ecf70c27
Spidy Boy <strcpy123@gmail.com>
2021-02-22 10:07:46 +0530

    Extra import removed

--- Swing/src/userProfileController/ReturnProduct.java
+++ Swing/src/userProfileController/ReturnProduct.java
@@ -10,7 +10,6 @@ import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JButton;
